b3d364941780507791e1c67ace1f9206d59dd6b3
simos <simosporgiotis@gmail.com>
2024-09-23 10:01:10 +0000

    added users to side bar

--- project/app/views.py
+++ project/app/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def index(request, *args, **kwargs):
     if not request.user.is_authenticated:
         return redirect("login-user")
+    # Get all users
+    users = User.objects.filter(is_superuser=False)
+
     context = {
-        
+        'users': users  # Pass users to the template
     }
     return render(request, "index.html", context)
 
